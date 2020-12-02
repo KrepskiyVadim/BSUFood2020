713ccaf7469b7c4c335757d7a02a249a260a0ab2
Vadim <vadim.krepskiy@mail.ru>
2020-12-03 02:55:19 +0300

    input menu

--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
+use App\Repository\DishRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,8 +15,23 @@ class MenuController extends AbstractController
     /**
      * @Route("/menu", name="menu")
      */
-    public function index(): Response
+    public function index(DishRepository $dishRepository, CategoryRepository $categoryRepository, PaginatorInterface $paginator, Request $request): Response
     {
-        return $this->render('quizes_list/index.html.twig');
+        $dishes = $dishRepository->findAll();
+        $categories = $categoryRepository->findAll();
+        $result1 = $paginator->paginate(
+            $dishes,
+            $request->query->getInt('page', 1),
+            $request->query->getInt('limit', 100000)
+        );
+        $result2 = $paginator->paginate(
+            $categories,
+            $request->query->getInt('page', 1),
+            $request->query->getInt('limit', 100000)
+        );
+        return $this->render('menu/index.html.twig', [
+            'dishes' => $result1, 'categories'=>$result2,
+        ]);
     }
+
 }
