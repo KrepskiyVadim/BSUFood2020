a12c1976cdcbc534565e6d6fc1ab2ba2521634d9
Vadim <vadim.krepskiy@mail.ru>
2020-12-04 11:17:39 +0300

    setting input menu

--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Repository\CategoryRepository;
+use App\Repository\DishCompositionRepository;
 use App\Repository\DishRepository;
+use App\Repository\IngredientRepository;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,10 +17,11 @@ class MenuController extends AbstractController
     /**
      * @Route("/menu", name="menu")
      */
-    public function index(DishRepository $dishRepository, CategoryRepository $categoryRepository, PaginatorInterface $paginator, Request $request): Response
+    public function index(DishRepository $dishRepository, CategoryRepository $categoryRepository, DishCompositionRepository $dishCompositionRepository, PaginatorInterface $paginator, Request $request): Response
     {
         $dishes = $dishRepository->findAll();
         $categories = $categoryRepository->findAll();
+        $dishCompositions = $dishCompositionRepository->findAll();
         $result1 = $paginator->paginate(
             $dishes,
             $request->query->getInt('page', 1),
@@ -29,8 +32,13 @@ class MenuController extends AbstractController
             $request->query->getInt('page', 1),
             $request->query->getInt('limit', 100000)
         );
+        $result3 = $paginator->paginate(
+            $dishCompositions,
+            $request->query->getInt('page', 1),
+            $request->query->getInt('limit', 100000)
+        );
         return $this->render('menu/index.html.twig', [
-            'dishes' => $result1, 'categories'=>$result2,
+            'dishes' => $result1, 'categories'=>$result2, 'dishCompositions' => $result3
         ]);
     }
 
