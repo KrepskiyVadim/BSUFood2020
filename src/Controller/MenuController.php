40027eae9e7e7f83bb3aa82b26befc42df9d4294
Vadim <vadim.krepskiy@mail.ru>
2020-12-04 14:43:38 +0300

    input header for admin and user

--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -22,23 +22,29 @@ class MenuController extends AbstractController
         $dishes = $dishRepository->findAll();
         $categories = $categoryRepository->findAll();
         $dishCompositions = $dishCompositionRepository->findAll();
-        $result1 = $paginator->paginate(
+        $result_dish = $paginator->paginate(
             $dishes,
             $request->query->getInt('page', 1),
             $request->query->getInt('limit', 100000)
         );
-        $result2 = $paginator->paginate(
+        $result_category = $paginator->paginate(
             $categories,
             $request->query->getInt('page', 1),
             $request->query->getInt('limit', 100000)
         );
-        $result3 = $paginator->paginate(
+        $result_dishComposition = $paginator->paginate(
             $dishCompositions,
             $request->query->getInt('page', 1),
             $request->query->getInt('limit', 100000)
         );
+        if(in_array("ROLE_ADMIN",$this->getUser()->getRoles()))
+            $parent_template = 'adminBase.html.twig';
+        else if(in_array("ROLE_USER",$this->getUser()->getRoles()))
+           $parent_template = 'homeBase.html.twig';
+        else $parent_template = 'base.html.twig';
         return $this->render('menu/index.html.twig', [
-            'dishes' => $result1, 'categories'=>$result2, 'dishCompositions' => $result3
+            'dishes' => $result_dish, 'categories'=>$result_category, 'dishCompositions' => $result_dishComposition,
+            'parent_template'=>$parent_template
         ]);
     }
 
